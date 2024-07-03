ab8858d6a3053aa79bd0099887ed2364acdc7395
duc hiep <hiep100524@gmail.com>
2024-07-03 21:00:14 +0700

    update

--- src/components/modal/ModalAuth.js
+++ src/components/modal/ModalAuth.js
@@ -11,8 +11,8 @@ const ModalAuth = () => {
     const token = Cookies.get('token');
     const navigate = useNavigate()
     const handleLoginGoogle = () => {
-        // window.location.href = 'https://movie-server-v2.onrender.com/oauth2/authorization/google';
-                window.location.href = 'http://localhost:8080/oauth2/authorization/google';
+        window.location.href = 'https://movie-server-v2.onrender.com/oauth2/authorization/google';
+                // window.location.href = 'http://localhost:8080/oauth2/authorization/google';
 
     };
     
